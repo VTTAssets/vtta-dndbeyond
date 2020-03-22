1a8f940e76832822a49e5b8c95e0d41e891bd576
Jack Thomas <jack@mrprimate.co.uk>
2020-03-22 22:23:32 +0000

    Spell Import: Create Saving Throw Spell Type
    This fixes the import to save spells on the character sheet as Saving
    Throw based rather than attack base, if they are listed as such.
    
    If a spell requires a saving throw and an attack, it defaults to attack.
    The logic in this case will still add a saving throw.
    
    I tested this with the Ice Dagger spell.

--- src/parser/character/spells.js
+++ src/parser/character/spells.js
@@ -175,6 +175,13 @@ let getRange = data => {
 
 let getActionType = data => {
   if (
+    data.definition.requiresSavingThrow &&
+    !data.definition.requiresAttackRoll
+  ) {
+    return "save";
+  }
+
+  if (
     data.definition.tags.includes("Damage") &&
     data.definition.range.rangeValue &&
     data.definition.range.rangeValue > 0
