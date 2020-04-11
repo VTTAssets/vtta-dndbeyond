fc4da416c2c56db7c1d0a4ba4c89ef1d21b446a4
Jack Thomas <jack@mrprimate.co.uk>
2020-04-11 20:09:43 +0100

    Spell Scalling Fixes
    Changes to spell scalling to make it more thorough, added some comments
    to help me understand it and now works for a lot more usecases!

--- src/parser/character/spells.js
+++ src/parser/character/spells.js
@@ -346,47 +346,127 @@ let getSave = data => {
 let getSpellScaling = (data, character) => {
   let baseDamage = "";
   let scaleDamage = "";
-  let scaleType = "";
+  let scaleType = null; //defaults to null, so will be picked up as a None scaling spell.
 
-  data.definition.modifiers
-    .filter(
-      mod =>
-        mod.type === "damage" ||
-        (mod.type === "bonus" && mod.subType === "hit-points")
-    )
-    .forEach(mod => {
-      if (mod && mod.die) {
-        if (mod.die.diceString !== null) {
-          baseDamage = mod.die.diceString;
-        }
+  // spell scaling
+  if (data.definition.canCastAtHigherLevel) {
+    // iterate over each spell modifier
+    data.definition.modifiers
+      .filter(
+        mod =>
+          mod.type === "damage" ||
+          (mod.type === "bonus" && mod.subType === "hit-points")
+      )
+      .forEach(mod => {
+        // if the modifier has a die for damage, lets use the string or fixed value
+        // for the base damage
+        if (mod && mod.die) {
+          if (mod.die.diceString !== null) {
+            baseDamage = mod.die.diceString;
+          }
 
-        if (mod.die.fixedValue !== null && baseDamage === "") {
-          baseDamage = mod.die.fixedValue;
-        }
-      }
-      if (data.definition.canCastAtHigherLevel) {
-        if (mod.atHigherLevels) {
-          scaleType = mod.atHigherLevels.scaleType;
-          if (
-            ["characterlevel", "spellscale"].includes(scaleType) &&
-            mod.atHigherLevels.higherLevelDefinitions &&
-            Array.isArray(mod.atHigherLevels.higherLevelDefinitions) &&
-            mod.atHigherLevels.higherLevelDefinitions.length >= 1
-          ) {
-            let definition = mod.atHigherLevels.higherLevelDefinitions[0];
-
-            if (definition) {
-              scaleDamage =
-                definition.dice && definition.dice.diceString
-                  ? definition.dice.diceString
-                  : definition.dice && definition.dice.fixedValue
-                  ? definition.dice.fixedValue
-                  : definition.value;
-            }
+          if (mod.die.fixedValue !== null && baseDamage === "") {
+            baseDamage = mod.die.fixedValue;
           }
         }
-      }
+
+        // defines some details about higher level casting
+        if (mod.atHigherLevels) {
+          // scaleTypes:
+          // SPELLSCALE - typical spells that scale
+          // SPELLLEVEL - these spells have benefits that come in at particular levels e.g. bestow curse, hex. typically  duration changes
+          // CHARACTERLEVEL - typical cantrip based levelling, some expections (eldritch blast)
+
+          let modScaleType = mod.atHigherLevels.scaleType;
+          // mod.atHigherLevels.higherLevelDefinitions contains info about the
+          // spells damage die at higher levels, but we can't use this for cantrips as
+          // FVTT use a formula to work out the scaling (ddb has a fixed value structure)
+          const isHigherLevelDefinitions = 
+            mod.atHigherLevels.higherLevelDefinitions &&
+            Array.isArray(mod.atHigherLevels.higherLevelDefinitions) &&
+            mod.atHigherLevels.higherLevelDefinitions.length >= 1;
+
+          // lets handle normal spell leveling first
+          if (
+            isHigherLevelDefinitions &&
+            modScaleType === "spellscale"
+          ) {
+            const definition = mod.atHigherLevels.higherLevelDefinitions[0];
+            if (definition) {
+              const modScaleDamage =
+                definition.dice && definition.dice.diceString // if dice string
+                  ? definition.dice.diceString // use dice string
+                  : definition.dice && definition.dice.fixedValue //else if fixed value
+                  ? definition.dice.fixedValue // use fixed value
+                  : definition.value; // else use value
+              
+             // some spells have multiple scaling damage (e.g. Wall of Ice, 
+              // Glyph of warding, Acid Arrow, Arcane Hand, Dragon's Breath,
+              // Chromatic Orb, Absorb Elements, Storm Sphere, Spirit Guardians)
+              // it's hard to model most of these in FVTT, and for some it makes
+              // no difference. so...
+              // lets optimistically use the highest
+              // assumptions: these are going to be dice strings, and we don't care
+              // about dice value, just number of dice
+              const diceForumula = /(\d*)d\d*/;
+              const existingMatch = diceForumula.exec(scaleDamage);
+              const modMatch = diceForumula.exec(modScaleDamage);
+
+              if (!existingMatch || modMatch[1] > existingMatch[1]) {
+                scaleDamage = modScaleDamage;
+              }
+              // finally update scaleType
+              scaleType = modScaleType;
+
+            } else {
+              console.warn("No definition found for " + data.definition.name);
+            }
+
+          } else if(
+            modScaleType === "spellscale"
+          ) {
+            // lets handle cases where there is a spellscale type but no damage
+            // increase/ higherleveldefinitins e.g. chain lighting
+            // these type of spells typically increase targets so we set the
+            // scaling to null as we don't want to increase damage when upcast.
+            // this also deals with cases like Ice Knife where the upscale damage
+            // is in one of the two mods provided.
+            // we are capturing this else because we don't want to trigger
+            // an update to scaleType or a warning.
+          } else if (modScaleType === "characterlevel") {
+            // lets handle odd cantrips like Eldritch Blast
+            // (in fact this might be the only case)
+            if (mod.atHigherLevels.higherLevelDefinitions.length === 0) {
+              // if this array is empty it does not contain levelling information
+              // the only case found is Eldritch Blast.
+              // this does have some info around multiple beams in
+              // data.atHigherLevels but we ignore this. we will set the scaling
+              // to null as each beam is best modelled by "casting" the cantrip again/
+              // pressing the attack/damage buttons in FVTT
+              scaleType = null;
+            } else {
+              scaleType = modScaleType;
+            }
+          } else if (modScaleType === "spelllevel"){
+            // spells that have particular level associated benefits
+            // these seem to be duration increases or target increases for
+            // the most part we can't handle these in FVTT right now (we could
+            // in theory create a new spell at a higher level).
+            // some duration upcasting (like bestow curse) affects concentration
+            // for now we will do nothing with these spells.
+            // examples include: hex, shadowblade, magic weapon, bestow curse
+            scaleType = modScaleType;
+          } else {
+            console.warn(
+              data.definition.name +
+              ' parse failed: ' +
+              JSON.stringify(modScaleType)
+            );
+            scaleType = modScaleType; // if this is new/unknow will use default
+          }
+        }
     });
+  }
 
   switch (scaleType) {
     case "characterlevel":
@@ -398,6 +478,12 @@ let getSpellScaling = (data, character) => {
       return {
         mode: "level",
         formula: scaleDamage
+      };
+    case "spelllevel":
+    case null:
+      return {
+        mode: "none",
+        formula: ""
       };
     default:
       return {
