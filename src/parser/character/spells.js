09a59bae17c3d597f6617b82e4556ff64c1f72c9
Jack Thomas <jack@mrprimate.co.uk>
2020-04-30 16:05:47 +0100

    Spell Target Parsing- regex out target numbers

--- src/parser/character/spells.js
+++ src/parser/character/spells.js
@@ -233,10 +233,23 @@ let getTarget = data => {
   let units = null;
   let value = null;
 
-  const creature = /(a creature you|creature( that)? you can see|interrupt a creature)|would strike a creature|creature of your choice|creature or object within range|cause a creature|creature must be within range/i;
-  const creaturesRange = /(humanoid|monster|creature|target)(s)? (or loose object )?(of your choice )?(that )?(you can see )?within range/i;
-  const creatures = creature.exec(data.definition.description) ||
-    creaturesRange.exec(data.definition.description);
+  const creature = /a creature you|creature( that)? you can see|interrupt a creature|would strike a creature|creature of your choice|creature or object within range|cause a creature|creature must be within range/gi;
+  const creaturesRange = /(humanoid|monster|creature|target)(s)? (or loose object )?(of your choice )?(that )?(you can see )?within range/gi;
+  const creatures = data.definition.description.match(creature) ||
+    data.definition.description.match(creaturesRange);
+
+  if (creatures) {
+    const numCreatures = /(?!At Higher Levels.*)(\w*) (falling )?(willing )?(creature|target|monster|celestial|fiend|fey|corpse(s)? of|humanoid)(?!.*you have animated)/gmi;
+    const targets = [...data.definition.description.matchAll(numCreatures)];
+    const targetValues = targets
+      .filter(target => {
+        const matches = DICTIONARY.numbers.filter(n => n.natural === target[1].toLowerCase())
+        return Array.isArray(matches) && !!matches.length;
+      })
+      .map(target => DICTIONARY.numbers.find(n => n.natural === target[1].toLowerCase()).num)
+
+    if (Array.isArray(targetValues) && !!targetValues.length) value = Math.max(...targetValues);
+  }
 
   switch (data.definition.range.origin) {
     case "Touch":
@@ -271,7 +284,7 @@ let getTarget = data => {
   };
 
   return {
-    value: value, // dd beyond doesn't let us know how many folk a spell can target
+    value: value,
     units: units, 
     type: type,
   };
