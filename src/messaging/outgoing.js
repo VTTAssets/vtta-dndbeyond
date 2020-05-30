337bcfebaabdc2273759236a634a33b7b32f2f8e
Jack Thomas <jack@mrprimate.co.uk>
2020-05-30 23:36:18 +0100

    Final Messaging Linting

--- src/messaging/outgoing.js
+++ src/messaging/outgoing.js
@@ -1,7 +1,7 @@
 // Port reactions or sending stuff to dndbeyond
 import utils from '../utils.js';
 
-let OutgoingCommunication = function(port) {
+let OutgoingCommunication = (port) => {
   return {
     updateActorHP: (actor, updateData) => {
       if (actor.data.type === 'character') {
@@ -18,7 +18,7 @@ let OutgoingCommunication = function(port) {
           utils.log(`Sending HP change for Actor ${actor.name}`, 'messaging');
           utils.log(data);
           port.send('set', data).then((response) => {
-            utils.log(`REceived response for HP change for Actor ${actor.name}`, 'communication');
+            utils.log(`Received response for HP change for Actor ${actor.name}`, 'communication');
             utils.log(response);
           });
         }
