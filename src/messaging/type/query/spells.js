c028fbd3f7154ce497acf2980e10f6e3e491d631
Sebastian Will <vttassets@gmail.com>
2020-06-09 13:23:26 +0200

    Change spell query to batch query the compendium

--- src/messaging/type/query/spells.js
+++ src/messaging/type/query/spells.js
@@ -4,16 +4,16 @@ const querySpells = async (message) => {
   let compendium = await game.settings.get("vtta-dndbeyond", "entity-spell-compendium");
 
   if (Array.isArray(message.name)) {
-    let results = {};
-
-    await Promise.all(message.name.map(async (name) => {
-      const details = await utils.queryCompendium(compendium, name);
-      results[name] = details;
-    }));
-
-    return { spell: results };
+    let details = await utils.queryCompendiumEntries(compendium, message.name);
+    // prepare the expected object
+    let result = {};
+    let idx = 0;
+    for (let spellName of message.name) {
+      result[spellName] = details[idx++];
+    }
+    return { spell: result };
   } else {
-    const details = await utils.queryCompendium(compendium, message.name);
+    const details = await utils.queryCompendiumEntry(compendium, message.name);
     return { spell: details };
   }
 };
