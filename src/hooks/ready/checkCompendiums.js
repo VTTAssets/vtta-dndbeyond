dfb2d33e469710fd907ae2c3c4bfe103b1beb263
Charles Hunter <charles082986@gmail.com>
2020-08-26 19:32:27 -0500

    Corrected TSLint rule violations

--- src/hooks/ready/checkCompendiums.js
+++ src/hooks/ready/checkCompendiums.js
@@ -1,11 +1,18 @@
 export default async function () {
-  let compendiumCreated = false;
+  let sanitize = (text) => {
+    if (text && typeof text === "string") {
+      return text.replace(/\s/g, '-').toLowerCase();
+    }
+    return text;
+  };
 
   let createIfNotExists = async (settingName, compendiumType, compendiumLabel) => {
     let compendiumName = game.settings.get("vtta-dndbeyond", settingName);
     let compendium = game.packs.find((pack) => pack.collection === compendiumName);
     let sanitizedLabel = sanitize(compendiumLabel);
-    if (compendium) { return false; }
+    if (compendium) {
+      return false;
+    }
     // create a compendium for the user
     await Compendium.create({
       entity: compendiumType,
@@ -17,21 +24,14 @@ export default async function () {
     return true;
   };
 
-  let sanitize = (text) => {
-    if (text && typeof text === "string") {
-      return text.replace(/\s/g, '-').toLowerCase();
-    }
-    return text;
-  }
-
   let results = await Promise.allSettled([
     createIfNotExists("entity-spell-compendium", "Item", "Spells"),
     createIfNotExists("entity-item-compendium", "Item", "Items"),
     createIfNotExists("entity-feature-compendium", "Item", "Features"),
     createIfNotExists("entity-monster-compendium", "Actor", "Monsters"),
-    //createIfNotExists("entity-class-compendium", "Item", "Classes"),
+    // createIfNotExists("entity-class-compendium", "Item", "Classes"),
     createIfNotExists("entity-monster-feature-compendium", "Item", "Monster Features")
   ]);
 
-  if (results.some(result => result.value)) location.reload();
+  if (results.some((result) => result.value)) location.reload();
 }
