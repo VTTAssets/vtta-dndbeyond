da0c73019a5652a065a153b261f8b51f2259b7ca
Charles Hunter <charles082986@gmail.com>
2020-08-28 15:23:51 -0500

    Fixing tslint rule violations

--- src/logger.js
+++ src/logger.js
@@ -9,56 +9,55 @@ const logger = {
         const setting = game.settings.get("vtta-dndbeyond", "log-level");
         const logLevels = ["VERBOSE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL", "OFF"];
         const logLevelIndex = logLevels.indexOf(logLevel);
-        if (setting == "OFF"
-            || logLevelIndex === -1
-            || logLevelIndex < logLevels.indexOf(setting)) {
+        if (setting == "OFF" ||
+            logLevelIndex === -1 ||
+            logLevelIndex < logLevels.indexOf(setting)) {
             return;
         }
-        if (logger._showLogMessage(logLevel, msg, payload)) {
-            const LOG_PREFIX = "VTTA D&D Beyond";
-            let msg = "No logging message provided.  Please see the payload for more information."
-            let payload = data.slice();
-            if (data[0] && typeof (data[0] == 'string')) {
-                msg = data[0];
-                if (data.length > 1) {
-                    payload = data.slice(1);
+
+        const LOG_PREFIX = "VTTA D&D Beyond";
+        let msg = "No logging message provided.  Please see the payload for more information.";
+        let payload = data.slice();
+        if (data[0] && typeof (data[0] == 'string')) {
+            msg = data[0];
+            if (data.length > 1) {
+                payload = data.slice(1);
+            } else {
+                payload = null;
+            }
+        }
+        msg = `${LOG_PREFIX} | ${logLevel} > ${msg}`;
+        switch (logLevel) {
+            case "VERBOSE":
+            case "DEBUG":
+                if (payload) {
+                    console.debug(msg, ...payload);// eslint-disable-line no-console
                 } else {
-                    payload = null;
+                    console.debug(msg);// eslint-disable-line no-console
                 }
-            }
-            msg = `${LOG_PREFIX} | ${logLevel} > ${msg}`;
-            switch (logLevel) {
-                case "VERBOSE":
-                case "DEBUG":
-                    if (payload) {
-                        console.debug(msg, ...payload)// eslint-disable-line no-console
-                    } else {
-                        console.debug(msg);// eslint-disable-line no-console
-                    }
-                    break;
-                case "INFO":
-                    if (payload) {
-                        console.info(msg, ...payload)// eslint-disable-line no-console
-                    } else {
-                        console.info(msg);// eslint-disable-line no-console
-                    }
-                    break;
-                case "WARN":
-                    if (payload) {
-                        console.warn(msg, ...payload)// eslint-disable-line no-console
-                    } else {
-                        console.warn(msg);// eslint-disable-line no-console
-                    }
-                    break;
-                case "FATAL":
-                case "ERROR":
-                    if (payload) {
-                        console.error(msg, ...payload)// eslint-disable-line no-console
-                    } else {
-                        console.error(msg);// eslint-disable-line no-console
-                    }
-                    break;
-            }
+                break;
+            case "INFO":
+                if (payload) {
+                    console.info(msg, ...payload);// eslint-disable-line no-console
+                } else {
+                    console.info(msg);// eslint-disable-line no-console
+                }
+                break;
+            case "WARN":
+                if (payload) {
+                    console.warn(msg, ...payload);// eslint-disable-line no-console
+                } else {
+                    console.warn(msg);// eslint-disable-line no-console
+                }
+                break;
+            case "FATAL":
+            case "ERROR":
+                if (payload) {
+                    console.error(msg, ...payload);// eslint-disable-line no-console
+                } else {
+                    console.error(msg);// eslint-disable-line no-console
+                }
+                break;
         }
     },
 
